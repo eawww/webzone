dbcaf6b0395056a3f521f3d057ef1d313ba3134f
Eric Wilson <eawww0@gmail.com>
2019-09-24 21:30:19 -0500

    Basic Homepage mashed together (#1)
    Just a guy out here trying to throw together a homepage. (#1)
    
    * 🚚 Move Stars into their own component
    
    * ⌨️ Add fonts
    
    * ➕ Add some deps
    
    * ✨ Home page thing did
    
    * 🦈 Oh no I added a context
    
    * 🐛 Some nonsense with image queries

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,40 +9,12 @@ import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { ScreenSizeProvider } from "../contexts/ScreenSize"
 import Header from "./header"
+import Stars from "./stars"
 import "./layout.css"
 
-const NUM_STARS = 500
-const MAX_STAR_R = 0.3 // Percent
-
-let keyBullshit = 0
-
-const starCoords = Array.from(new Array(NUM_STARS)).map(() => ({
-  x: Math.random() * 100,
-  y: Math.random() * 100,
-  r: Math.random(),
-  key: keyBullshit++,
-}))
-
 const Layout = ({ children }) => {
-  const [screenHeight, setScreenHeight] = useState(0)
-  const [screenWidth, setScreenWidth] = useState(0)
-
-  useEffect(() => {
-    const resetWindowDimensions = () => {
-      setScreenHeight(window.innerHeight)
-      setScreenWidth(window.innerWidth)
-    }
-    resetWindowDimensions()
-    window.addEventListener(
-      "resize",
-      () => {
-        resetWindowDimensions()
-      },
-      true
-    )
-  }, [])
-
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -54,29 +26,13 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <svg
-        className="svgCanvasStars"
-        viewBox={`0 0 ${screenWidth} ${screenHeight}`}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {starCoords.map(star => (
-          <circle
-            key={star.key}
-            cx={`${star.x}%`}
-            cy={`${star.y}%`}
-            r={`${star.r * MAX_STAR_R}%`}
-            strokeWidth="0"
-            fill="#FFF"
-          ></circle>
-        ))}
-      </svg>
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <ScreenSizeProvider>
+      {/* <Stars /> */}
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div>
         <main>{children}</main>
-        <footer>© {new Date().getFullYear()}, Eric Wilson</footer>
       </div>
-    </>
+    </ScreenSizeProvider>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
 
 import Layout from "../components/layout"
 import "./index.css"
 import SEO from "../components/seo"
-import underConstruction from "../images/under-construction-flashing-lights.gif"
+import MeOnALion from "../components/MeOnALion"
 
 const NUM_STARS = 500
 
 const IndexPage = () => (
   <Layout>
     <SEO title="🏚" />
-    <marquee height="50%">
-      <img className="underConstruction" src={underConstruction} />
-    </marquee>
+    <MeOnALion />
+    <div className="theTops">
+      <h1 className="nameHeader">Eric Wilson</h1>
+      <p className="subtitle">
+        I develop web like Peter Parker in the darkroom.
+      </p>
+    </div>
+    <hr className="gradientDivider"></hr>
+    <div className="theBusiness">
+      <h2>Me:</h2>
+      <p>Software Engineer by day; sleepy Web Developer by night.</p>
+      <p>Grower and eater of plants.</p>
+      <p>
+        I have a motorcycle so you know I'm really cool.
+        <br />
+        (vroom vroom 🏍)
+      </p>
+    </div>
   </Layout>
 )
 
