1ef896c25bc6a88de4e62443171f4bb2cfd01f62
Eric Wilson <eawww0@gmail.com>
2020-06-24 22:17:01 -0500

    🌈Add kickass new dark mode, fix responsiveness (#3)
    * 🌈 Add kickass new dark mode, fix responsiveness
    
    * 🐞 Remember to start it in light mode
    
    * 🌌 Add stars to dark sky

---
+++ src/components/MeOnALionColors.js
@@ -0,0 +1,28 @@
+const COLORS = {
+  spookyFace: {LIGHT: '#0000', DARK: '#20ff00'},
+  clouds1: {LIGHT: '#bbb', DARK: '#777'},
+  clouds2: {LIGHT: '#ededeb', DARK: '#b9e231'},
+  rock: {LIGHT: "#f1c584", DARK: "#222"},
+  head: {LIGHT: "#e59162", DARK: "#898"},
+  beard: {LIGHT: "#3B2519", DARK: "#443"},
+  hair1: {LIGHT: "#3B2519", DARK: "#443"},
+  hair2: {LIGHT: "#6B4835", DARK: "#667"},
+  sunglassLenses: {LIGHT: "#29282F", DARK: "#000"},
+  sunglassFrames: {LIGHT: "#F4DCA9", DARK: "cyan"},
+  noustrils: {LIGHT: "#2f1c10", DARK: "#000"},
+  upperLip: {LIGHT: "#ab4d3b", DARK: "red"},
+  lowerLip: {LIGHT: "#ed8562", DARK: "hotPink"},
+  shirt: {LIGHT: "#f7b1ad", DARK: "#590b6b"},
+  shirtBG: {LIGHT: "#89e1e3", DARK: "#555"},
+  legallyDistinctStarfishMan: {LIGHT: "#ff785e", DARK: "#d808ca"},
+  shirtHoles: {LIGHT: "#ff785e", DARK: "#000"},
+  shorts: {LIGHT: "#efd63b", DARK: "#fc7814"},
+  shortsFlowers: {LIGHT: "#5f3e45", DARK: "#000"},
+  arms: {LIGHT: "#ec9a62", DARK: "#789178"},
+  legs: {LIGHT: "#feb980", DARK: "#89a389"},
+  shoes: {LIGHT: "#5e5146", DARK: "#93b51b"},
+  shoeSoles: {LIGHT: "#f6e7cf", DARK: "#f6e7cf"},
+  shoeLaces: {LIGHT: "#f6e7cf", DARK: "#f6e7cf"}
+}
+
+export default COLORS;
